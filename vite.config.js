9492928daff6d2616348767dfb4f24c049e75b24
AkashBag2004 <akash.bag@iemlabs.com>
2025-09-04 18:11:57 +0530

    Second commit

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+    react(),
+    VitePWA({
+      manifest: {
+        name: 'My React PWA',
+        short_name: 'ReactPWA',
+        icons: [
+          { src: 'icon-192x192.png', sizes: '192x192', type: 'image/png' },
+          { src: 'icon-512x512.png', sizes: '512x512', type: 'image/png' }
+        ]
+      }
+    })
+  ]
+});
